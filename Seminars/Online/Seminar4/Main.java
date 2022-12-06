a3a13201a5676a5cc3464ede3faf6ad0045f2cb9
1Nekey1 <lenleolem@yandex.ru>
2022-12-06 18:24:22 +0800

    06.12.22

---
+++ Seminars/Online/Seminar4/Main.java
@@ -0,0 +1,19 @@
+package Seminars.Online.Seminar4;
+
+public class Main {
+    public static void main(String[] args) {
+//        FirstProblem firstProblem = new FirstProblem();
+//        firstProblem.arrayListTime();
+//        firstProblem.linkedListTime();
+
+//        SecondProblem secondProblem = new SecondProblem();
+//        secondProblem.addToLinkedList();
+
+//        ThirdProblem thirdProblem = new ThirdProblem();
+//        thirdProblem.task();
+
+//        FourthProblem fourthProblem = new FourthProblem();
+//        fourthProblem.addToStack();
+//        fourthProblem.addToQueue();
+    }
+}
